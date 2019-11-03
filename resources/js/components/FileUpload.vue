19b6d9d02241f575ff5da90d4722209925493d71
Michaelkimani100 <michaelkimani854@gmail.com>
2019-11-04 00:01:37 +0300

    frontendsetup

---
+++ resources/js/components/FileUpload.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center mt-3">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header"><h3>File upload</h3></div>
+                    <div class="card-body">
+                        <form>
+                        <div class="form-group">
+                         <label for="exampleFormControlFile1">Select File</label>
+                         <input type="file" class="form-control">
+                        </div>
+                          <button type="submit" class="btn btn-primary float-right">Submit</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
+<style scoped>
+.card{
+    background-color: whitesmoke;
+}
+.card-header{
+    color: #b08f26;
+}
+.btn{
+    background-color:#b08f26;
+    border: none;
+}
+</style>
